de32ea3f674cdbf16e14e423a24887e2fdf2d89e
Richard Eisenberg <rae@richarde.dev>
2024-09-01 22:01:53 +0200

    Locals demos

---
+++ locals/basics.ml
@@ -0,0 +1,67 @@
+(* Locals cannot escape their region.
+   Functions bodies (along with loops) are regions.*)
+
+(* So f1 here doesn't work (try it!): *)
+
+(*
+let f1 () =
+  let x @ local = "hello" in
+  x
+*)
+
+(* Changing the "local" there to "global" (or omitting the mode annotation
+   entirely) fixes the problem. *)
+
+(* Globals cannot refer to locals: allowing that would allow you
+   to smuggle a local out from a function. *)
+
+(* Thus f2 here doesn't work (try it!): *)
+
+(*
+let f2 () =
+  let x @ local = "hello" in
+  let _xs @ global = [x] in
+  ()
+*)
+
+(* On the other hand, storing a global in a local is just fine: *)
+
+let f3 () =
+  let x @ global = "hello" in
+  let _xs @ local = [x] in
+  ()
+
+(* The "no escape" property of locals allows us to write a safe
+   [with_file] construct: *)
+
+module With_file : sig
+  type file_handle
+  val with_file : filename:string -> f:(file_handle @ local -> 'r @ global) -> 'r @ global
+
+  (* The following [@ local] annotation is necessary to say that
+     [read_file] promises not to save the handle anywhere. *)
+  val read_file : file_handle @ local -> string
+end = struct
+  type file_handle
+  let with_file ~filename:_ ~f:_ = failwith "unimplemented"
+  let read_file _handle = failwith "unimplemented"
+end
+
+(* This access pattern is OK: *)
+let f4 () =
+  let open With_file in
+  with_file ~filename:"data.txt" ~f:(fun handle ->
+      let contents = read_file handle in
+      ignore contents)
+
+(* This access pattern is not (try it!): *)
+
+(*
+let f5 () =
+  let open With_file in
+  let handle_ref = ref None in
+  with_file ~filename:"data.txt" ~f:(fun handle ->
+      (* store it in the ref: *)
+      handle_ref := Some handle);
+  !handle_ref
+*)
---
+++ locals/stack.ml
@@ -0,0 +1,70 @@
+(* Look at [basics.ml] before looking here. *)
+
+(* Because local values do not escape their function, we can use
+   a stack allocation discipline to back deallocating local
+   values independent of the garbage collector. *)
+
+(* This file defines a program that generates a matrix filled
+   with random integers. It then multiplies each integer in the
+   matrix with the index of its row. That way, when we print, we
+   can easily see whether the operation happened (because all the
+   numbers will be multiples of the row index). Note that doing
+   this operation requires nested iterations, where the inner
+   iteration makes a closure that captures the row index.
+
+   If we use reimplementations of the stdlib Array functions that
+   note their function argument as [local], then we know that the
+   functions passed to [iter] and [iteri] can be stack allocated.
+
+   Compile this program (with
+     > ocamlopt -o stack stack.ml
+   ) and then run it, producing GC statistics (with
+     > OCAMLRUNPARAM="v=0x400" ./stack
+   ). Observe the difference bwtween using the stdlib array functions
+   and our localized equivalents. *)
+
+let size = 20
+
+module Local = struct
+  module Array = struct
+    let init size (f @ local) =
+      if size = 0 then [| |] else
+      let first = f 0 in
+      let arr = Array.make size first in
+      for index = 1 to size - 1 do
+        arr.(index) <- f index
+      done;
+      arr
+
+    let iteri (f @ local) arr =
+      for index = 0 to Array.length arr - 1 do
+        f index arr.(index)
+      done
+
+    (* The [@nontail[ annotation here is a tricky bit. Come talk to
+       us about it if you're interested! *)
+    let iter (f @ local) arr = iteri (fun _index -> f) arr [@nontail]
+
+    (* These are necessary because the syntax [a.(i)] is really just
+       a call to [Array.get a i] for whatever [Array] module is in scope,
+       and similarly for [set]. *)
+    let get = Array.get
+    let set = Array.set
+  end
+end
+
+let () =
+  (* Uncomment this next line: *)
+  (* let open Local in *)
+  Random.init 1;
+  let arr =
+    Array.init size (fun _index ->
+        Array.init size (fun _index ->
+            Random.int 100))
+  in
+  Array.iteri (fun r inner ->
+      Array.iteri (fun c elt -> arr.(r).(c) <- elt * r) inner) arr;
+  Array.iteri (fun r inner ->
+      Printf.printf "%d: " r;
+      Array.iter (fun elt -> Printf.printf "%d " elt) inner;
+      Printf.printf "\n") arr
